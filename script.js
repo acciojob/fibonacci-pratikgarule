0bbfaf0f93fe7bfcd5b01c4a92731513d90facc2
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-24 11:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function fibonacci(num) {
-// your code here
-}
+let fib =[0,1]
+function fibonacci(num){
 
-module.exports = fibonacci;
+	for(let i=2;i<num;i++){
+		fib[i] = fib[i-1] + fib[i-2];
+	}
+	return fib[num-1]
+}
+module.exports = fibonacci
